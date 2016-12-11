bf66b014032b9c13516d65bcd844b84bbf6cb2c1
Flowmaster <ellyssin.gimhae@gmail.com>
2016-12-11 16:43:49 -0700

    Added sports book json body parser.

--- app/controllers/Arbiter.scala
+++ app/controllers/Arbiter.scala
@@ -5,6 +5,7 @@ import javax.inject.Inject
 
 import jp.t2v.lab.play2.auth.OptionalAuthElement
 import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.libs.json.{JsError, Json}
 import play.api.mvc.{Action, Controller}
 
 import scala.concurrent.Future
@@ -18,12 +19,28 @@ class Arbiter @Inject() (val database: DBService,
                          implicit val webJarAssets: WebJarAssets)
   extends Controller with AuthConfigTrait with OptionalAuthElement with I18nSupport  {
 
+  case class SportsEventOdds(europe: Double, american: Option[Int])
+  case class SportsEventLine(name: String, odds: SportsEventOdds)
+  case class SportsEvent(name: String, time: String, lines: Seq[SportsEventLine])
+  case class SportsBook(exchange: String, sport: String, events: Seq[SportsEvent])
+
+  implicit val oddsRead = Json.reads[SportsEventOdds]
+  implicit val lineRead = Json.reads[SportsEventLine]
+  implicit val eventRead = Json.reads[SportsEvent]
+  implicit val bookReads = Json.reads[SportsBook]
 
   /**
     * Post new sportsbook events here.
     */
-  def addEvents() = Action.async { implicit request =>
-    println(request.body.asJson)
-    Future.successful(Ok(""))
+  def addEvents() = Action.async( parse.json ) { implicit request =>
+    request.body.validate[SportsBook].fold (
+      errors => {
+        Future.successful(BadRequest(Json.obj("status" ->"KO", "message" -> JsError.toJson(errors))))
+      },
+      book => {
+        println(book)
+        Future.successful(Ok(Json.obj("status" ->"OK", "message" -> ("Received") )))
+      }
+    )
   }
 }
