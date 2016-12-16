b1c4762f640c0d1129c1594f5adc62d216eb7ae3
Flowmaster <ellyssin.gimhae@gmail.com>
2016-12-15 21:57:18 -0700

    Added a grid view for single match matrices.

--- app/controllers/Arbiter.scala
+++ app/controllers/Arbiter.scala
@@ -40,8 +40,39 @@ class Arbiter @Inject() (val database: DBService,
   /**
     * Should display a view of betting lines with odds for a specific sport.
     */
-  def sportLines(sportName: String) = Action.async { implicit request =>
+  def matrices(filter: Option[String]) = AsyncStack { implicit request =>
+    // test stuff
+    val matrix = new SportBookMatrix("NFL Football")
+    val part1 = "Cardinals"
+    val part2 = "Lions"
+    val bookName1 = "Nitrogen Sports"
+    val matchName = s"$part1 - $part2"
+    val nitroline1 = SportsEventLine(part1, 1.3)
+    val nitroline2 = SportsEventLine(part2, 2.3)
+    matrix.addMatchOdds(bookName1, matchName, nitroline1, nitroline2)
 
-    Future.successful(Ok(""))
+    val bookName2 = "Cloudbet"
+    val cloudbetline1 = SportsEventLine(part1, 3.3)
+    val cloudbetline2 = SportsEventLine(part2, 1.3)
+    matrix.addMatchOdds(bookName2, matchName, cloudbetline1, cloudbetline2)
+
+    val bookName3 = "Betcoin"
+    val betline1 = SportsEventLine(part2, 1.1)
+    val betline2 = SportsEventLine(part1, 2.0)
+    matrix.addMatchOdds(bookName3, matchName, betline1, betline2)
+
+    val bookNames = matrix.bookNames
+    val matchs = matrix.matchNames
+    val (keya, keyb) = matchs.map{ name =>
+      val names = name.split(" - ").sorted
+      (names(0), names(1))
+    }.head
+
+    val odds = matrix.matchOdds(matchs.head).sortBy(_.bookname)
+    println(odds)
+    val linea = odds.map(_.a)
+    val lineb = odds.map(_.b)
+
+    Future.successful(Ok(views.html.arbiter.grid(loggedIn, bookNames, keya, keyb, linea, lineb)))
   }
 }
